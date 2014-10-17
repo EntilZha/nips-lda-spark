4f776d1afa9d8d8cb9ea73a762b4d33b84858d1c
Pedro Rodriguez <ski.rodriguez@gmail.com>
2014-10-17 10:13:54 -0700

    Fixed bug where I was filtering words which didn't match [A-Za-z] instead of stripping out special characters

--- src/main/scala/NipsLda.scala
+++ src/main/scala/NipsLda.scala
@@ -2,7 +2,7 @@ package nipslda
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.graphx.algorithms.LDA
+import org.apache.spark.graphx.lib.LDA
 import scala.io
 
 object Main {
@@ -14,27 +14,24 @@ object Main {
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     conf.set("spark.kryo.registrator", "org.apache.spark.graphx.GraphKryoRegistrator")
     val sc = new SparkContext(conf)
-    val docs = sc.wholeTextFiles("data/nipstxt/**")
     val stopWords = io.Source.fromFile("data/stop-words.txt").getLines().map(l => l.trim()).toSet
-    val tokens = docs.flatMap({case (name, contents) => contents.split(' ').map(w => w.trim().toLowerCase)})
-    assert(stopWords.contains("the"))
+    val docs = sc.wholeTextFiles("data/nipstxt/**").map({case (name, contents) =>
+      (name, contents.replaceAll("[^A-Za-z']+", " ").trim.toLowerCase.split("\\s+").filter(w => !stopWords(w)))
+    })
+    val tokens = docs.flatMap({case (name, contents) => contents})
     val (vocab, vocabLookup) = LDA.extractVocab(tokens)
-    for (e <- stopWords) {
-      println(e)
-    }
-    val regex = "[A-Za-z]+".r
     val edges = docs.flatMap({case (name, contents) =>
-      contents.split(' ').map(w => w.trim().toLowerCase).filter(w => !stopWords.contains(w) && regex.pattern.matcher(w).matches).map(
-        token => (vocabLookup(token.toLowerCase), Math.abs(name.hashCode).asInstanceOf[LDA.DocId])
+      contents.map(
+        token => (vocabLookup(token), Math.abs(name.hashCode).asInstanceOf[LDA.DocId])
       )
     })
-    val model = new LDA(edges, 10)
+    val model = new LDA(edges, 50)
     model.iterate(10)
-    val words = model.topWords(5)
+    val words = model.topWords(15)
     sc.stop()
-    for (i <- 0 to 9) {
+    for (i <- 0 to 49) {
       print("Topic " + i.toString + ": ")
-      for (w <- 0 to 4) {
+      for (w <- 0 to 14) {
         val word = vocab(words(i)(w)._2.toInt)
         val count = words(i)(w)._1
         print(count.toString + "*" + word + " ")
