0a8e6b8aa66ff8de58066edd0ed50b438888c171
Pedro Rodriguez <ski.rodriguez@gmail.com>
2014-11-14 13:15:33 -0800

    removed bad call to wholeTextFiles

--- src/main/scala/NipsLda.scala
+++ src/main/scala/NipsLda.scala
@@ -10,13 +10,9 @@ object NipsLda {
   def edgesVocabFromText(sc:SparkContext):
                         (RDD[(LDA.WordId, LDA.DocId)], Array[String], Map[String, LDA.WordId]) = {
     val stopWords = io.Source.fromFile("/root/nips-lda-spark/data/stop-words.txt").getLines().map(l => l.trim()).toSet
-    val docs = sc.wholeTextFiles("s3n://files.sparks.public/data/enwiki_category_text/part-00000").map({case (name, contents) =>
-      (name, contents.replaceAll("[^A-Za-z']+", " ").trim.toLowerCase.split("\\s+").filter(w => !stopWords(w)))
+    val docs = sc.textFile("s3n://files.sparks.public/data/enwiki_category_text/part-00000").map({contents =>
+      (contents.hashCode(), contents.replaceAll("[^A-Za-z']+", " ").trim.toLowerCase.split("\\s+").filter(w => !stopWords(w)))
     })
-
-    //val docs = sc.textFile("s3n://files.sparks.public/data/enwiki_category_text/part-00000").map({contents =>
-    //  (contents.hashCode(), contents.replaceAll("[^A-Za-z']+", " ").trim.toLowerCase.split("\\s+").filter(w => !stopWords(w)))
-    //})
 
     val tokens = docs.flatMap({case (name, contents) => contents})
     val (vocab, vocabLookup) = LDA.extractVocab(tokens)
